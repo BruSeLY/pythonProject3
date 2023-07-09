d1e992c1c1648143ef674b3f9691fd2cd03f7df3
BruSeLY <103708209+BruSeLY@users.noreply.github.com>
2023-07-09 18:24:27 +0900

    Update dota2predict.py

--- dota2predict.py
+++ dota2predict.py
@@ -12,27 +12,48 @@ matches_not_final = {}
 r = r.html.links
 print(r)
 matches = {}
-t =0
+t = 0
 for i in r:
     if len(i) == 19:
         matches_not_final[t] = i[9:]
-        t+=1
+        t += 1
 
 heroes = requests.get("https://api.opendota.com/api/heroes").json()
 print(heroes)
 with open ("heroes.txt", "w") as f:
     for i in range(len(heroes)):
-        f.write(f'{heroes[i]["localized_name"]} = {heroes[i]["id"]}\n')
+        if i != len(heroes) - 1:
+            f.write(f'{heroes[i]["id"]}={heroes[i]["localized_name"]}\n')
+        else:
+            f.write(f'{(heroes[i]["id"])}={heroes[i]["localized_name"]}')
+
 
 response = requests.get('https://api.opendota.com/api/matches/' + matches_not_final[0]).json()
 print(response)
 print(response["match_id"])
-print(response["picks_bans"])
-# for j in range(10):
-#     if response['picks_bans'][j][
-#         matches[response["match_id"]] = f"{response['rad']}radiant_team: {response['picks_bans']"
+radiant = []
+dire = []
+for j in range(10):
+    if response['picks_bans'][j]["team"] == 0:
+        radiant.append(str(response["picks_bans"][j]["hero_id"]))
+    else:
+        dire.append(str(response["picks_bans"][j]["hero_id"]))
 
-# print(matches)
+print(radiant)
+sl = dict()
+with open ("heroes.txt", "r") as f:
+    s = f.read().split("\n")
+    for row in s:
+        row = row.split("=")
+        sl[row[0]] = row[1]
+print(sl)
+radiantWin = response["radiant_win"]
+for i in range(5):
+    radiant[i] = sl[radiant[i]]
+for i in range(5):
+    dire[i] = sl[dire[i]]
+print(radiantWin)
+print(radiant, "vs", dire)
 
 
 
